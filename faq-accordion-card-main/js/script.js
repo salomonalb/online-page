13ddaf05dd2718859a989b183ca7927ca7dbb5a5
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-07-24 22:11:30 -0400

    added typescript, fixing some css issues

--- faq-accordion-card-main/js/script.js
+++ faq-accordion-card-main/js/script.js
@@ -1,1 +1,1 @@
-let accordion;
+const accordions=document.getElementsByClassName("faq-card__accordion");for(const c of accordions)c.addEventListener("click",(()=>{c.children[0].classList.toggle("question-open"),c.children[1].classList.toggle("icon-open"),c.children[2].classList.toggle("answer-open")}));
--- faq-accordion-card-main/ts/script.ts
+++ faq-accordion-card-main/ts/script.ts
@@ -1,1 +1,15 @@
-let accordion = document.getElementById();
+/* Getting all the elements with the class name of faq-card__accordion. */
+const accordions = document.getElementsByClassName('faq-card\_\_accordion');
+
+/* This is a for loop that is looping through the accordions. It is adding an event listener to each
+accordion. When the accordion is clicked, it is toggling the classList of the children of the
+accordion. */
+for (const accordion of accordions) {
+    accordion.addEventListener('click', () => {
+        accordion.children[0].classList.toggle('question-open');
+        accordion.children[1].classList.toggle('icon-open');
+        accordion.children[2].classList.toggle('answer-open')
+    })
+}
+
+
