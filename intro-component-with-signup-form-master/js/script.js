49ba6de9b2a71d336401e1b511c7d8302f533e09
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-08-05 01:39:54 -0400

    some javascript

--- intro-component-with-signup-form-master/js/script.js
+++ intro-component-with-signup-form-master/js/script.js
@@ -1,1 +1,34 @@
-"use strict";
+const form = document.getElementById('form');
+const nameInput = document.getElementById('name');
+const lastnameInput = document.getElementById('lastname');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const submitButton = document.getElementById('submit');
+
+
+const formInputs = [nameInput, lastnameInput, emailInput, passwordInput]
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (nameInput.value.length < 1) {
+        nameInput.classList.add('--error');
+    }
+    if (lastnameInput.value.length < 1) {
+        lastnameInput.classList.add('--error');
+    }
+    if (passwordInput.value.length < 1) {
+        passwordInput.classList.add('--error');
+    }
+    if (emailInput.value.length < 1) {
+        emailInput.classList.add('--error');
+    }
+})
+
+
+/* regular expression for the email */
+/* onchange event for the inputs to add or remove the error class
+but onlyafter trying to submite once, maybe add the event listener inside the first event listener for submitting the form. */
+/* if values are correct submit the form */ 
+
