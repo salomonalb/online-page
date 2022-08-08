075ae4c04a9a353c356bf8dcfbffae089a7b845b
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-08-08 18:34:15 -0400

    animations

--- ping-coming-soon-page-master/js/script.js
+++ ping-coming-soon-page-master/js/script.js
@@ -3,7 +3,7 @@ const emailInput = document.getElementById('email');
 const form = document.getElementById('form');
 
 emailInput.addEventListener('input', event => {
-    emailInput.value.length >= 5
+    emailInput.value.length > 3
     ? input.classList.remove('error')
     : input.classList.add('error')
 })
