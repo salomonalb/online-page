271a4ab775c8a3df7738b4c9043617e84ff759de
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-08-08 18:30:22 -0400

    input validation class

--- ping-coming-soon-page-master/js/script.js
+++ ping-coming-soon-page-master/js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+const emailInput = document.getElementById('email');
+const form = document.getElementById('form');
+
+emailInput.addEventListener('input', event => {
+    emailInput.value.length >= 5
+    ? input.classList.remove('error')
+    : input.classList.add('error')
+})
+
+emailInput.addEventListener('invalid', () => {
+    emailInput.classList.add('error');
+})
+
+form.addEventListener('submit', (event) => {
+
+    if (emailInput.value.length < 4) {
+        emailInput.classList.add('error');
+        event.preventDefault();
+    }
+})
+
