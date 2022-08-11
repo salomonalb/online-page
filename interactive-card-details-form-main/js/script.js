06afa58019e9bac609746f533a338da505309e6a
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-08-11 00:29:59 -0400

    finished the styles

--- interactive-card-details-form-main/js/script.js
+++ interactive-card-details-form-main/js/script.js
@@ -1,1 +1,11 @@
 "use strict"
+
+/* get the input elements, add an input event that checks if the value inside the input element matches the format or regular expression, could be the validity state,
+if they are invalid, add the error class to the input element and the respctive alert. if they are valid then remove the class from both. 
+
+/*get the elements of the cards and uptade its text content with the value of the inputs */
+
+/* when submiting the form check if every value in the input elements is valid, if one its not valid prevent the default of the submit element and add a class error to that input element and its alert.  if everything is correct then pudate the html to show the thankyou component. when they press the continue button it should refresh the page to show the form again.*/
+
+
+
