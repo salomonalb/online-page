4bad93ee327f4d01776774f3a08d1cc69c9b60c1
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-08-31 00:09:45 -0400

    mobile header

--- sunnyside-agency-landing-page-main/js/script.js
+++ sunnyside-agency-landing-page-main/js/script.js
@@ -1,1 +1,13 @@
 "use strict"
+
+const button = document.querySelector('#button');
+const nav = document.querySelector('#nav');
+
+button.addEventListener('click', () => {
+    if(nav.style.display === 'block') {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = 'block';
+    }
+    
+})
