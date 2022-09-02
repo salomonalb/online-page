b4fa4e7eb8e83ca523114fcbab3a159132f99ee6
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-09-02 19:39:03 -0400

    css config changed

--- sunnyside-agency-landing-page-main/js/script.js
+++ sunnyside-agency-landing-page-main/js/script.js
@@ -1,13 +1,20 @@
 "use strict"
 
-const button = document.querySelector('#button');
+const btn = document.querySelector('#button');
 const nav = document.querySelector('#nav');
 
-button.addEventListener('click', () => {
+btn.addEventListener('click', () => {
     if(nav.style.display === 'block') {
         nav.style.display = "none";
     } else {
         nav.style.display = 'block';
     }
-    
+});
+
+window.addEventListener('resize', ()=> {
+    if (window.innerWidth >= 769 ) {
+        nav.style.display = "flex";
+    } else {
+        nav.style.display = "none";
+    }
 })
