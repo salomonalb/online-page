ccbfdab059a88245d64ed75802a778e42e023eed
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-09-17 22:45:22 -0400

    finished

--- interactive-pricing-component-main/js/script.js
+++ interactive-pricing-component-main/js/script.js
@@ -1,38 +1,78 @@
+"use strict"
 
-//Radio Button
-const radioBtn = document.getElementById('radio');
-let yearly;
-function toggle() {
-    if (radioBtn.getAttribute('data-yearly') === 'false') {
-        radioBtn.setAttribute('data-yearly', 'true')
+const pageViews = document.getElementById('pageviews');
+const slider = document.getElementById('slider');
+const progressBar = document.getElementById('progress');
+const price = document.getElementById('price');
+const check = document.getElementById('check');
+const discount = document.getElementById('discount');
 
-        yearly = true
-    } else {
-        radioBtn.setAttribute('data-yearly', 'false')
-
-        yearly = false
-    }
-    console.log(radioBtn.getAttribute('data-yearly'))
+function changeNumbers(pageViewsString, priceString) {
+    pageViews.textContent = pageViewsString;
+    price.textContent = priceString;
 }
 
-radioBtn.addEventListener('click', ()=> {
-    toggle()
-});
+function handleInput() {
+    let sliderPosition = Number(slider.value);
 
-radioBtn.addEventListener('keydown', (event) => {
-    if (event.code === "Enter") {
-        toggle()
+    if (!check.checked) {
+        switch (sliderPosition) {
+            case 1:
+                changeNumbers('10K', 8);
+                progressBar.style.width = '0%';
+                break;
+            case 2:
+                changeNumbers('50K', 12);
+                progressBar.style.width = '25%';
+                break;
+            case 3:
+                changeNumbers('100K', 16);
+                progressBar.style.width = '50%';
+                break;
+            case 4:
+                changeNumbers('500K', 24);
+                progressBar.style.width = '75%';
+                break;
+            case 5:
+                changeNumbers('1M', 36);
+                progressBar.style.width = '100%';
+                break;
+            default:
+                break;
+        }
+    } else {
+        switch (sliderPosition) {
+            case 1:
+                changeNumbers('10K', 6);
+                progressBar.style.width = '0%';
+                break;
+            case 2:
+                changeNumbers('50K', 9);
+                progressBar.style.width = '25%';
+                break;
+            case 3:
+                changeNumbers('100K', 12);
+                progressBar.style.width = '50%';
+                break;
+            case 4:
+                changeNumbers('500K', 18);
+                progressBar.style.width = '75%';
+                break;
+            case 5:
+                changeNumbers('1M', 27);
+                progressBar.style.width = '100%';
+                break;
+            default:
+                break;
+        }
     }
-})
 
-/*https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/switch_role*/
+}
 
-// -------------------------
+slider.addEventListener('input', handleInput)
 
+check.addEventListener('input', handleInput)
 
-// -------------
-
-const discount = document.getElementById('discount');
 
 if (window.innerWidth >= 480) {
     discount.textContent = '25% discount'
